7c5b6f3d0aac3307daf7d6201b3763ff8594abfc
Jatin Gupta <43518631+JatinGupta27@users.noreply.github.com>
2019-07-24 00:04:47 +0530

    Update signup.php

--- signup.php
+++ signup.php
@@ -9,8 +9,8 @@ $username_err = $password_err = $confirm_password_err = "";
 // Processing form data when form is submitted
 if($_SERVER["REQUEST_METHOD"] == "POST"){
  
-    // Validate username
-    if(empty(trim($_POST["username"]))){
+    // Validation
+ if(empty(trim($_POST["username"]))){
         $username_err = "Please enter a username.";
     } else{
         // Prepare a select statement
